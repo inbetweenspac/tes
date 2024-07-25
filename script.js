5ce80f7690e4e2ffdde43f059ac85d6b6bce33ab
inbetweenspac <maksamahatma@gmail.com>
2024-07-25 19:17:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 ;(function () {
-  // NOTE: These are more flexible when they are arrays (thus, Array.from())
   var btnEls = Array.from(document.querySelectorAll('.button'))
   var msgEls = Array.from(document.querySelectorAll('messagesDisplay'))
 
   function handleButtonClick(event) {
     var btnEl = event.target
-    btnEl.disabled = true // optional
-    btnEl.dataset.toggled = 'true' // using the DOM to hold data
+    btnEl.disabled = true 
+    btnEl.dataset.toggled = 'true' 
 
     if (btnEls.some(function (el) {
       return el.dataset.toggled !== 'true'
@@ -17,7 +16,6 @@
     })
   }
 
-  // Our "onclick" equivalent
   btnEls.forEach(function (el) {
     el.addEventListener('click', handleButtonClick)
   })
